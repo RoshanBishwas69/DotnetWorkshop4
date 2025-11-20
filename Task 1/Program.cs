9409267dd9492c83df931560ac7a535561bf8e7d
CURDMaster24 <bishwasroshan11@gmail.com>
2025-11-20 08:30:28 +0545

    Task 1 Completed

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Task_1
+﻿namespace workshop4;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Student s1 = new Student();
+        s1.Name = "Stephen";
+        s1.Age = 25;
+
+        Student s2 = new Student();
+        s2.Name = "kim Jong Un";
+        s2.Age = 50;
+
+        Console.WriteLine("Student 1:");
+        Console.WriteLine("Name: " + s1.Name);
+        Console.WriteLine("Age: " + s1.Age);
+        Console.WriteLine("School: " + Student.SchoolName);
+
+        Console.WriteLine("Student 2:");
+        Console.WriteLine("Name: " + s2.Name);
+        Console.WriteLine("Age: " + s2.Age);
+        Console.WriteLine("School: " + Student.SchoolName);
     }
 }
