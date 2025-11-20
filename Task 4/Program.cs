e62be47157be1ee499ec06954ad6d749d9818a99
CURDMaster24 <bishwasroshan11@gmail.com>
2025-11-20 09:01:21 +0545

    Done

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Task_4
+﻿namespace Task4;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
+        Player player1 = new Player();
+        player1.playerName = "USA";
+        player1.level = 100;
+        player1.health = "very good";
+
+
+        Player player2 = new Player("Canada", 10, "Good");
+        Console.WriteLine($"Player 1: Name = {player1.playerName}, Level = {player1.level}, Health = {player1.health}");
+        Console.WriteLine($"Player 2: Name = {player2.playerName}, Level = {player2.level}, Health = {player2.health}");
+
     }
 }
